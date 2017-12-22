102660585157c2ff4d9d86ab452d451f7d934acb
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-12-23 02:47:21 +0700

    init

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+	void OnTriggerEnter (Collider col)
+	{
+		GameObject explosion = Instantiate(Resources.Load("FlareMobile", typeof(GameObject))) as GameObject;
+		explosion.transform.position = transform.position;
+		//Destroy(col.gameObject);
+		Destroy (explosion, 2);
+
+
+		if (col.gameObject.tag == "Player") {
+			Destroy (col.gameObject);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerScript : MonoBehaviour {
+
+	public GameObject imageTarget;
+	public Button fireButton;
+	public float speed = 250f;
+
+	// Use this for initialization
+	void Start () {
+		fireButton.onClick.AddListener (OnButtonDown);	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnButtonDown () {
+		GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
+		Rigidbody rb = bullet.GetComponent<Rigidbody>();
+		bullet.transform.rotation = imageTarget.transform.rotation;
+		bullet.transform.position = imageTarget.transform.position;
+		rb.AddForce(Camera.main.transform.forward * speed);
+		bullet.GetComponent<AudioSource>().Play ();
+		Destroy (bullet, 3);
+	}
+}
